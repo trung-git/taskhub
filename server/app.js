cbf368023ba7cb543dba124b05d34efb64385ae8
Trung Thanh Huynh <trunght1810@gmail.com>
2023-06-23 13:35:15 +0700

    [Server] Init global error handling

--- server/app.js
+++ server/app.js
@@ -12,9 +12,19 @@ app.use(cookieParser());
 //Router
 const userRouter = require('./routes/userRoutes.js');
 
+// Utils
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError.js');
+
 app.get('/', (req, res) => {
   res.status(200).json({ data: 'App is running....' });
 });
 app.use('/api/v1/user', userRouter);
 
+app.all('*', (req, res, next) => {
+  next(new AppError(`Not found ${req.originalUrl}`, 404));
+});
+//Error handling
+app.use(globalErrorHandler);
+  
 module.exports = app;
