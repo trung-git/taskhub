efdb1a3ee824ac99ba25d122550c064f2c6b21f7
Trung Thanh Huynh <trunght1810@gmail.com>
2023-06-23 13:37:57 +0700

    [Server] Refactor code for auth module

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,95 +1,63 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.signup = async (req, res, next) => {
-  try {
-    const newUser = await User.create(req.body);
-
-    const token = jwt.sign({ _id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    });
-
-    const cookieOptions = {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-    };
-    if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
-      cookieOptions.secure = true;
-    }
-    res.cookie('jwt', token, cookieOptions);
-    newUser.password = undefined;
-
-    return res.status(200).json({
-      success: true,
-      token,
-      data: {
-        newUser,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      error: error,
-    });
-  }
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 };
 
-exports.login = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-
-    // 1) Check if email and password exist
-    if (!username || !password) {
-      return res.status(400).json({
-        error: 'Please provide email and password',
-      });
-    }
-    // 2) Check if user exists && password is correct
-    const user = await User.findOne({ username }).select('+password');
-
-    if (!user || !(await user.correctPassword(password, user.password))) {
-      return res.status(401).json({
-        error: 'Incorrect email or password',
-      });
-    }
-
-    // 3) If everything ok, send token to client
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    });
-
-    const cookieOptions = {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-    };
-    if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
-      cookieOptions.secure = true;
-    }
-    res.cookie('jwt', token, cookieOptions);
-    user.password = undefined;
-
-    return res.status(200).json({
-      success: true,
-      token,
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message,
-      stack: error.stack,
-    });
+const createAndSendToken = (user, statusCode, req, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
+    cookieOptions.secure = true;
   }
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined;
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
 };
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+  createAndSendToken(newUser, 201, req, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!username || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ username }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3) If everything ok, send token to client
+  createAndSendToken(user, 200, req, res);
+});
 
 exports.logout = (req, res) => {
   res.cookie('jwt', 'logout', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
   });
-  res.status(200).json({ success: true });
+  res.status(200).json({ status: 'success' });
 };
