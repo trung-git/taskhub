33ab6afb0e2de1ea9e34b3bdc540b83f60fce59b
Trung Thanh Huynh <trunght1810@gmail.com>
2023-07-12 19:18:30 +0700

    [Server] Add restrict method

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -234,3 +234,16 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   //Log the user in, set JWT
   createAndSendToken(user, 200, req, res);
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['Finder', 'Tasker']. role='Finder'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+
+    next();
+  };
+};
