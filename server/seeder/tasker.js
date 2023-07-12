2c4951a7fe1822d50c819c207deea5bcea093244
Trung Thanh Huynh <trunght1810@gmail.com>
2023-07-12 19:17:30 +0700

    [Server] Update tasker seeder

--- server/seeder/tasker.js
+++ server/seeder/tasker.js
@@ -3,6 +3,79 @@ const bcrypt = require('bcryptjs');
 
 const taskers = [
   {
+    username: 'trunghuynh',
+    firstName: 'Trung',
+    lastName: 'Huynh',
+    dateOfBirth: '1993-02-11',
+    email: 'trunght1810@gmail.com',
+    phoneNumber: '0123456789',
+    gender: 'Male',
+    image: '',
+    password: bcrypt.hashSync('123456789', 10),
+    workLocation: [
+      mongoose.Types.ObjectId.createFromHexString('64a129fd40878eb40868ebd0'),
+    ],
+    taskTag: [
+      {
+        taskInfo: mongoose.Types.ObjectId.createFromHexString(
+          '64a0163810f4179aa8f4c7b3'
+        ),
+        price: 7,
+      },
+    ],
+    workTime: [
+      {
+        date: '',
+        start: '08:00',
+        end: '17:00',
+      },
+      {
+        date: '',
+        start: '08:00',
+        end: '17:00',
+      },
+      {
+        date: '',
+        start: '08:00',
+        end: '17:00',
+      },
+      {
+        date: '',
+        start: '08:00',
+        end: '17:00',
+      },
+      {
+        date: '',
+        start: '08:00',
+        end: '17:00',
+      },
+      {
+        date: '',
+        start: '08:00',
+        end: '17:00',
+      },
+      {
+        date: '',
+        start: '08:00',
+        end: '17:00',
+      },
+    ],
+    reviews: [
+      mongoose.Types.ObjectId.createFromHexString('64a029cd505314a289b1c18d'),
+    ],
+    profile: {
+      aboutMe:
+        'Ex tempor exercitation aliqua eiusmod. Sint exercitation exercitation Lorem velit labore proident Lorem quis fugiat. Velit Lorem consequat laboris adipisicing ex ut aliqua ut et et qui. Sint cillum cupidatat non proident do in esse amet ex ullamco id cillum deserunt. Esse velit irure sint veniam mollit aute aliqua eu aliquip Lorem ad. Consectetur velit consectetur fugiat amet pariatur eiusmod.',
+      skillAndExperience:
+        'Magna dolor officia consequat cupidatat nostrud. In proident ex irure reprehenderit do adipisicing sunt ullamco mollit proident dolor voluptate elit. Eu adipisicing elit enim elit nulla laborum. Mollit fugiat consectetur cillum elit nisi cupidatat occaecat. Ea deserunt exercitation laborum enim aute et aliqua aliqua adipisicing incididunt est laboris. Incididunt est et proident excepteur reprehenderit eiusmod proident laboris consequat anim sint mollit consectetur. Ipsum tempor id aliquip magna ad non pariatur qui Lorem quis cupidatat consequat enim.',
+      photo: [],
+      vehicle: 'Bike',
+    },
+    averageRating: 5,
+    contracts: [],
+    isVerified: true,
+  },
+  {
     username: 'bao1',
     firstName: 'Bao',
     lastName: 'Nguyen',
