df9f1d6dd175f739f331efb3817ea8948502274d
Trung Thanh Huynh <trunght1810@gmail.com>
2023-08-15 23:57:39 +0700

    Revert "[Server] Add header credential"
    This reverts commit 5c5bacf1670ceb5d37570ac19463f39e2f8c4b7b.

--- server/app.js
+++ server/app.js
@@ -22,15 +22,6 @@ app.use(
     tempFileDir: '/tmp/',
   })
 );
-app.use(function(req, res, next) {
-  res.header('Content-Type', 'application/json;charset=UTF-8')
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
 
 //Router
 const userRouter = require('./routes/userRoutes.js');
