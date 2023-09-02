3b34d6795ab02834f424281f429885113d847906
Lê Hiền Lương <75094831+luongic@users.noreply.github.com>
2023-09-02 14:57:30 +0700

    [WEB] - config build

--- webclient/taskhub/src/base/config.js
+++ webclient/taskhub/src/base/config.js
@@ -1,4 +1,5 @@
 // export const API_URL = 'https://taskhub-mhm7.onrender.com/';
+// export const SOCKET_SERVER = 'https://taskhub-mhm7.onrender.com';
 export const API_URL = 'http://localhost:8000/';
 export const SOCKET_SERVER = 'http://localhost:8000';
 
