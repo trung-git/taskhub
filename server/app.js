45b3a2e9626eecb18631148437ec6b00a7158e4b
Trung Thanh Huynh <trunght1810@gmail.com>
2023-09-07 23:01:43 +0700

    [Server] update socket

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://taskhub-pro.netlify.app'],
+  origin: '*',
   credentials: true,
 };
 
