51a9c486f3fa66c5cb05e307e47e24092dc2d9ed
Trung Thanh Huynh <trunght1810@gmail.com>
2023-09-09 02:30:54 +0700

    [Web Client] Remove Strict mode

--- webclient/taskhub/src/index.js
+++ webclient/taskhub/src/index.js
@@ -11,7 +11,7 @@ import { SocketProvider } from './provider/SocketContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <ConfigProvider>
       <BrowserRouter>
         <SocketProvider>
@@ -21,7 +21,7 @@ root.render(
         </SocketProvider>
       </BrowserRouter>
     </ConfigProvider>
-  </React.StrictMode>
+  /* </React.StrictMode> */
 );
 
 // If you want to start measuring performance in your app, pass a function
