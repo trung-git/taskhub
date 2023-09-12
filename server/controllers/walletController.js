098b23f2a183df7f9e12325378fc07f9d5881b37
Trung Thanh Huynh <trunght1810@gmail.com>
2023-09-12 16:06:37 +0700

    [Server] Update wallet api

--- server/controllers/walletController.js
+++ server/controllers/walletController.js
@@ -43,7 +43,7 @@ exports.action = catchAsync(async (req, res, next) => {
     }
     wallet.amount -= amount;
   }
-  wallet.paymentHistory = [...wallet.paymentHistory].push(payment);
+  wallet.paymentHistory = [...wallet.paymentHistory, payment];
 
   const savedWallet = await wallet.save();
 
